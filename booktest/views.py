1f07e50fd8ae0733e2da337bbafbaf278fce421e
xtbo97 <xtbo9719@gmail.com>
2020-12-14 10:46:19 +0800

    英雄人物图书展示-接口4:修改指定的英雄人物数据(根据英雄ID)+接口5:删除指定的英雄人物数据(根据英雄ID)

--- booktest/views.py
+++ booktest/views.py
@@ -9,7 +9,7 @@ class HeroListView(View):
     """ 英雄列表 获取 新增 视图 """
 
     def get(self, request):
-        """ 英雄列表获取"""
+        """ 获取所有的英雄人物数据 """
         heros_list = []
         heros = HeroInfo.objects.all()
         for hero in heros:
@@ -29,7 +29,7 @@ class HeroListView(View):
         return JsonResponse(response_dict)
 
     def post(self, request):
-        """ 英雄新增"""
+        """ 新增一个英雄人物数据 """
         req_dict = json.loads(request.body)
         hname = req_dict.get("hname")
         hgender = req_dict.get("hgender")
@@ -56,7 +56,7 @@ class HeroDetailView(View):
     """ 英雄详情 获取 修改 删除 视图"""
 
     def get(self, request, id):
-        """ 获取指定英雄人物数据"""
+        """ 获取指定的英雄人物数据(根据英雄ID) """
         try:
             hero = HeroInfo.objects.get(id=id)
         except HeroInfo.DoesNotExist:
@@ -80,8 +80,52 @@ class HeroDetailView(View):
             }
             return JsonResponse(success_response_dict)
 
-    def put(self, request):
-        pass
+    def put(self, request, id):
+        """ 修改指定的英雄人物数据(根据英雄ID) """
+        try:
+            hero = HeroInfo.objects.get(id=id)
 
-    def delete(self, request):
-        pass
+        except HeroInfo.DoesNotExist:
+            fail_response_dict = {
+                "code": 1,
+                "message": f"id为{id}的英雄不存在"
+            }
+            return JsonResponse(fail_response_dict)
+        else:
+            req_dict = json.loads(request.body)
+            hero.hname = req_dict.get("hname")
+            hero.hgender = req_dict.get("hgender")
+            hero.hcomment = req_dict.get("hcomment")
+            hero.hbook_id = req_dict.get("hbook_id")
+            hero.save()
+            success_response_dict = {
+                "code": 0,
+                "message": "OK",
+                "hero": {
+                    "id": hero.id,
+                    "hname": hero.hname,
+                    "hgender": hero.hgender,
+                    "hcomment": hero.hcomment,
+                    "hbook": hero.hbook.btitle,
+                    "hbook_id": hero.hbook_id,
+                }
+            }
+            return JsonResponse(success_response_dict)
+
+    def delete(self, request, id):
+        """ 删除指定的英雄人物数据(根据英雄ID) """
+        try:
+            hero = HeroInfo.objects.get(id=id)
+        except HeroInfo.DoesNotExist:
+            fail_response_dict = {
+                "code": 1,
+                "message": f"id为{id}的英雄不存在"
+            }
+            return JsonResponse(fail_response_dict)
+        else:
+            success_response_dict = {
+                "code": 0,
+                "message": "OK",
+            }
+            hero.delete()
+            return JsonResponse(success_response_dict)
